87086048920bee4e3664c6e8fe3d10f6963f7845
Nick Bansal <extreme_wired@msn.com>
2019-05-15 20:09:28 +0100

    refactored code

--- MarsRover/utils.js
+++ MarsRover/utils.js
@@ -1,27 +1,23 @@
 /* eslint-disable no-undef */
+const { MarsRover, RoverInstructions } = require("./index");
 const printNextGeneration = (grid) => {
     const gridCoords = grid[0].map(Number);
     let roverName = [];
     let instructionsName = [];
+
     process.stdout.write("\n");
     for (let i = 1; i < grid.length; i += 2) {
-        roverIndex = 1;
-        roverName[roverIndex] = new MarsRover(Number(grid[i][0]), Number(grid[i][1]), grid[i][2]);
-        roverIndex++;
+        roverName[i] = new MarsRover(Number(grid[i][0]), Number(grid[i][1]), grid[i][2]);
     }
     for (let i = 2; i < grid.length; i += 2) {
-        instructionsIndex = 1;
-        instructionsName[instructionsIndex] = new RoverInstructions(gridCoords, roverName[instructionsIndex]);
-        console.log(instructionsName[instructionsIndex]);
-        process.stdout.write(`${instructionsName[instructionsIndex].instructions(grid[i].join(" "))}\n`);
-        // console.log(instructionsName[instructionsIndex].instructions(grid[i].join(" ")));
-        instructionsIndex++;
+        instructionsName[i] = new RoverInstructions(gridCoords, roverName[i - 1]);
+        process.stdout.write(`${instructionsName[i].instructions(grid[i].join(" "))}\n`);
     }
     process.stdout.write("\n");
     process.exit();
 };
 
-module.exports = printNextGeneration;
+module.exports = { printNextGeneration };
 
 
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const { MarsRover, RoverInstructions } = require("./MarsRover/index");
+const { printNextGeneration } = require("./MarsRover/utils");
 const grid = [];
 
 process.stdout.write(`\nPlease enter your grid coordinates below - MUST include spaces\n
@@ -9,22 +9,6 @@ process.stdin.on("data", (data) => {
     newData[0].toLowerCase() === "end" ? printNextGeneration(grid) : grid.push(newData);
 });
 
-const printNextGeneration = (grid) => {
-    const gridCoords = grid[0].map(Number);
-    let roverName = [];
-    let instructionsName = [];
 
-    process.stdout.write("\n");
-    for (let i = 1; i < grid.length; i += 2) {
-        roverName[i] = new MarsRover(Number(grid[i][0]), Number(grid[i][1]), grid[i][2]);
-    }
-
-    for (let i = 2; i < grid.length; i += 2) {
-        instructionsName[i] = new RoverInstructions(gridCoords, roverName[i - 1]);
-        process.stdout.write(`${instructionsName[i].instructions(grid[i].join(" "))}\n`);
-    }
-    process.stdout.write("\n");
-    process.exit();
-};
 
 
