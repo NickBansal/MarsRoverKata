756ed84fb85de84fad879c9df1c4ff63fbf6f573
Nick Bansal <extreme_wired@msn.com>
2019-05-16 11:30:19 +0100

    functions renamed

--- MarsRover/utils.js
+++ MarsRover/utils.js
@@ -1,18 +1,21 @@
 /* eslint-disable no-undef */
 const { MarsRover, RoverInstructions } = require("./index");
 
-const printNextGeneration = (grid) => {
-    const gridCoords = grid[0].map(Number);
+const printNextGeneration = (commands) => {
+    const gridCoords = commands[0].map(Number);
     let roverName = [];
     let instructionsName = [];
 
     process.stdout.write("\n");
-    for (let i = 1; i < grid.length; i += 2) {
-        roverName[i] = new MarsRover(Number(grid[i][0]), Number(grid[i][1]), grid[i][2]);
+    commands.forEach(() => {
+
+    });
+    for (let i = 1; i < commands.length; i += 2) {
+        roverName[i] = new MarsRover(Number(commands[i][0]), Number(commands[i][1]), commands[i][2]);
     }
-    for (let i = 2; i < grid.length; i += 2) {
+    for (let i = 2; i < commands.length; i += 2) {
         instructionsName[i] = new RoverInstructions(gridCoords, roverName[i - 1]);
-        process.stdout.write(`${instructionsName[i].instructions(grid[i].join(" "))}\n`);
+        process.stdout.write(`${instructionsName[i].instructions(commands[i].join(" "))}\n`);
     }
     process.stdout.write("\n");
     process.exit();
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 const printNextGeneration = require("./MarsRover/utils");
-const grid = [];
+const userCommands = [];
 
 process.stdout.write(`\nPlease enter your grid coordinates below - MUST include spaces\n
 Then enter your rover coordinates and orientation followed by 'end' for results\n\n`);
 process.stdin.on("data", (data) => {
     const newData = data.toString().trim().split(" ");
-    newData[0].toLowerCase() === "end" ? printNextGeneration(grid) : grid.push(newData);
+    newData[0].toLowerCase() === "end" ? printNextGeneration(userCommands) : userCommands.push(newData);
 });
 
 
